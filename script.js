799c5804beebb1cc79f2855bc19ddc1f9384a37e
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-03-09 14:16:57 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const pi = 3.14;
 
 function bereken() {
-    var input = document.querySelector("input").value;
-    var out = input * 3.14;
-    document.querySelector("p").innerHTML = out;
+    let input = document.querySelector("input").value;
+    var out = input * pi;
+    var out2 = input * input * pi * 0.25;
+    document.querySelectorAll("p")[0].innerHTML = out;
+    document.querySelectorAll("p")[1].innerHTML = out2;
 }
